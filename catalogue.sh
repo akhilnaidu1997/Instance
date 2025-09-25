068d130ed9c7d997b2fb2bb987fe9c2621e5b941
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-25 11:09:11 +0530

    message

--- catalogue.sh
+++ catalogue.sh
@@ -6,6 +6,7 @@ if [ $USERID -ne 0 ]; then
     echo "ERROR:: You donot have sudo permissions, please install with sudo permissions"
     exit 1 # Telling the script to stop here and dont go further 
 fi
+PWD=$PWD
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -16,17 +17,31 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+dnf module disable nodejs -y &>> output.log
+dnf module enable nodejs:20 -y &>> output.log
+dnf install nodejs -y &>> output.log
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> output.log
 
 mkdir /app 
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> output.log
 
-npm install 
+npm install &>> output.log
+
+cp $PWD /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue  &>> output.log
+systemctl start catalogue &>> output.log
+
+cp $PWD/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y &>> output.log
+mongosh --host mongodb.daws86s-akhil.shop </app/db/master-data.js &>> output.log
+
+
 
