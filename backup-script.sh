6c3591a59d29ff137ca585ba86668a8af9c35b9b
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-30 10:29:29 +0530

    message

--- backup-script.sh
+++ backup-script.sh
@@ -5,6 +5,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+SOURCE=$1
+DEST=$2
+DAYS=${3:-14}
+
 USER=$(id -u)
 
 LOG_FOLDER="/var/log/shell-practice"
@@ -27,4 +31,12 @@ USAGE(){
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d $SOURCE ]; then
+    echo -e " $R Error : Source $SOURCE does not exist"
+fi
+
+if [ ! -d $DEST ]; then
+    echo -e " $R Error : Destination $DEST does not exist"
 fi
