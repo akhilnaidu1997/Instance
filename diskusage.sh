8d509bcb1591aef6ab24e15dfcd3c35b76682aca
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-02 13:30:21 +0530

    message

--- diskusage.sh
+++ diskusage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 
 while IFS= read -r line
 do
-    USAGE=$(echo "$line" | awk '{print $6}')
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo "$line" | awk '{print $7}')
     echo "$PARTITION: $USAGE"
     
