7c75ace054af12844a62e2408c1fdc027dffe9f1
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-02 17:33:42 +0530

    message

--- disk2.sh
+++ disk2.sh
@@ -2,12 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
+MESSAGE=""
 
 while IFS= read -r line
 do
     USAGE=$(echo "$line" | awk '{print $6}'| cut -d "%" -f1)
     PARTITION=$(echo "$line" | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High usage $PARTITION: $USAGE"
+        MESSAGE+=$(echo "High usage $PARTITION: $USAGE")
     fi
 done <<< $DISK_USAGE
+
+echo "Message : $MESSAGE"
