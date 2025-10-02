43999bd364c2979d55a62d9bb05d83f60666d430
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-02 13:26:57 +0530

    message

--- diskusage.sh
+++ diskusage.sh
@@ -4,6 +4,7 @@ DISK_USAGE=$(df -h | grep -v Filesystem)
 
 while IFS= read -r line
 do
-    USAGE=$(echo "$line" | aws '{print $6}')
+    USAGE=$(echo "$line" | awk '{print $6}')
     
 done <<< $DISK_USAGE
+
